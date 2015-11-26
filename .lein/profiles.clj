a2396d179ccde46534600e4155f8cb50e69b011f
Wei "pw" Peng <write.to.peng.wei@gmail.com>
2015-11-26 12:38:18 -0800

    Update Leiningen profile config.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,17 +6,18 @@
                   ;; for CIDER - Clojure(Script) IDE
                   [cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.1.0"]
-                  [org.clojure/tools.nrepl "0.2.10"]
+                  [org.clojure/tools.nrepl "0.2.11"]
 
                   ;; other Clojure targets
-                  [lein-cljsbuild "1.0.6"]
-                  ;;[lein-droid "0.2.3"]
+                  [lein-cljsbuild "1.1.1"]
+                  [lein-figwheel "0.5.0-2"]
 
                   ;; better REPL
-                  [venantius/ultra "0.3.4"]
+                  ;;[venantius/ultra "0.4.0"]
+                  ;;[venantius/ultra "0.3.4"] ; last version that supports Clojure 1.6.x
                   
                   ;; misc plugins
-                  [lein-ring "0.9.6"]
+                  [lein-ring "0.9.7"]
                   [lein-localrepo "0.5.3"]
                   [lein-marginalia "0.8.0"]
                   [lein-javadoc "0.1.1"]
@@ -26,27 +27,30 @@
                   ]
 
         :dependencies [
+                       ;; utilities
+                       [im.chit/hara.reflect "2.2.11"] ; JVM reflections
+                       [alembic "0.3.2"] ; classpath reloading
+                       
                        ;; for ClojureScript REPL
-                       [org.clojure/clojurescript "1.7.48"]
                        [com.cemerick/piggieback "0.2.1"]
                        [weasel "0.7.0"]
 
                        [slamhound "1.5.5"]
-                       [alembic "0.3.2"]
+                       
                        ]
 
-        ;; https://github.com/cemerick/piggieback
-        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
-                       :init (do
-                               (use 'clojure.pprint
-                                    'clojure.stacktrace))}
+        
+        :repl-options {
+                       ;; https://github.com/cemerick/piggieback
+                       :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
 
-        ;; https://github.com/venantius/ultra/issues/13
-        :ultra {:repl true
-                :stacktraces true
-                :tests true
-                :java true
-                :color-scheme :solarized_dark}
+                       ;; REPL utilities
+                       :init (do
+                               (use 'clojure.pprint ; pprint print-table
+                                    'clojure.repl ; source doc apropos root-cause pst
+                                    'hara.reflect ; query-class query-instance
+                                    )
+                               (require '[alembic.still :refer [distill load-project]]))}
 
         ;; local JDK API javadoc
         ;; use Leiningen profile read-eval trick
